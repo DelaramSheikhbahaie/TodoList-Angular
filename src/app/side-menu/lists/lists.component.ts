7127d7e7f2138e68d226a26c1a120726177204eb
Delaram Sheikhbahaie <sheikhbahaied@gmail.com>
2021-11-14 17:22:13 +0330

    create side-menu

--- src/app/side-menu/lists/lists.component.ts
+++ src/app/side-menu/lists/lists.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {List} from '../../models/lists'
 @Component({
   selector: 'app-lists',
   templateUrl: './lists.component.html',
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListsComponent implements OnInit {
 
+  lists:List[];
+  inputListName:string = "";
+
   constructor() { }
 
   ngOnInit(): void {
+    this.lists = [
+      {
+        name :" list1"
+      }
+    ]
   }
+  deleteList (id:number){
+    this.lists = this.lists.filter((list , index)=> index !== id)
+  }
+  // editName (name:string){
 
+  // }
 }
