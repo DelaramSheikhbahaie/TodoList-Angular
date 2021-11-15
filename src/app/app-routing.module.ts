d5b562f27d19edb74a14e402145aad5cdc92d1a1
Delaram Sheikhbahaie <sheikhbahaied@gmail.com>
2021-11-15 12:39:52 +0330

    routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainPageComponent } from './main-page/main-page.component';
+import { ListsComponent } from './side-menu/lists/lists.component';
+import { SideMenuComponent } from './side-menu/side-menu.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"" , component:MainPageComponent},
+  {path:"lists" , component:SideMenuComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
